13e2921324c58430d5fa720128f58af616810476
Joel Grayson <joelgrayson@Joels-Laptop.lan>
2025-02-06 14:51:32 +0200

    day 4

--- week1/day 4/exercisesxp.py
+++ week1/day 4/exercisesxp.py
@@ -22,3 +22,49 @@ describe_city('Madrid')
 
 #Ex4
 
+import random
+
+def compare_numbers(user_number):
+    random_number = random.randint(1, 100)
+    
+    if user_number == random_number:
+        print('Success! The numbers match')
+    else:
+        print(f'Fail! The numbers do not match. Your number was {user_number}, and the random number was {random_number}.')
+
+compare_numbers(50)
+
+#Ex6
+
+magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+def show_magicians():
+    for magician in magician_names:
+        print(magician)
+
+def make_great():
+    for i in range(len(magician_names)):
+        magician_names[i] = magician_names[i] + ' the Great'
+
+make_great()
+
+show_magicians()
+
+#Ex5
+
+def make_shirt(size, message):
+    print(f"The size of the shirt is {size} and the text is '{message}'")
+
+make_shirt('Large', 'I Love Tel Aviv')
+
+def make_shirt(size='Large', message='I love Python'):
+    print(f"The size of the shirt is {size} and the text is '{message}'")
+
+make_shirt()
+
+make_shirt('Medium')
+
+make_shirt('Small', 'I love Haifa')
+
+
+
