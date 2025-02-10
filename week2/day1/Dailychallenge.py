e7cad35a51411207fa7c4e29bc7a97276fe39c8f
Joel Grayson <joelgrayson@Joels-Laptop.lan>
2025-02-10 14:13:28 +0200

    day1

---
+++ week2/day1/Dailychallenge.py
@@ -0,0 +1,36 @@
+class Farm:
+    def __init__(self, name):
+        self.name = name
+        self.animals = {}
+
+    def add_animal(self, animal, count=1):
+        if animal in self.animals:
+            self.animals[animal] += count
+        else:
+            self.animals[animal] = count
+
+    def get_info(self):
+        info = f"{self.name}'s farm\n"
+        for animal, count in self.animals.items():
+            info += f"{animal} : {count}\n"
+        info += "\n   E-I-E-I-0!"
+        return info
+
+    def get_animal_types(self):
+        return sorted(self.animals.keys())
+
+    def get_short_info(self):
+        animal_types = self.get_animal_types()
+        animals_sentence = ', '.join(animal_types)
+        animals_sentence = animals_sentence.replace("sheep", "sheeps")
+        return f"{self.name}'s farm has {animals_sentence}."
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+
+print(macdonald.get_info())
+
+print(macdonald.get_short_info())
